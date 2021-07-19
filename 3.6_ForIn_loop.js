0df8b6c72c0887f67c014c7fd3dfe69c9cfe738d
Aminul667 <rahathssn@gmail.com>
2021-07-19 20:06:22 +0600

    add section three

---
+++ 3.6_ForIn_loop.js
@@ -0,0 +1,13 @@
+
+const person = {
+    name: 'Rahat',
+    age: 26
+};
+
+for (let key in person)
+    console.log(key, person[key]);
+
+const colors = ['red', 'green', 'blue'];
+
+for (let index in colors)
+    console.log(index, colors[index]);
---
+++ 3.9_Max_Ex.js
@@ -0,0 +1,10 @@
+
+let number = max(3, 2);
+console.log(number);
+
+function max(a, b) {
+    // if (a > b) return a;
+    // return b;
+
+    return (a > b) ? a : b;
+}
